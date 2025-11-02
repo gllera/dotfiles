d81ce2caf680e120d534b5eb8220ca15328b31c6
gllera <gabriellleragarcia@gmail.com>
2025-11-02 19:31:00 +0000

    Rofi configuration restored. Add new scripts for system setup and i3 configuration; update .gitignore to include new directories

--- i3/system.sh
+++ i3/system.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-PID=$(notify-send -epu critical "System mode [ENABLED]")
-NOTIFY="notify-send -er $PID"
-trap "$NOTIFY -t 1000 'System mode [DISABLED]'" exit
-
-DMENU=(timeout 20s dmenu -i -sb '#8B0000' -sf '#FFD700')
-RES=$(printf '%s\n' suspend reboot poweroff reload.i3 restart.i3 exit.i3 | "${DMENU[@]}" -p System)
+ROFI=(timeout 20s rofi -dmenu -theme-str 'window {width: 20ch;}' )
+RES=$(printf '%s\n' suspend reboot poweroff i3-reload i3-restart i3-exit | "${ROFI[@]}" -p System)
 
 [[ -n $RES ]] || exit
-printf '%s\n' No Yes | "${DMENU[@]}" -p "(confirm) $RES" | grep -q Yes >/dev/null || exit
+printf '%s\n' No Yes | "${ROFI[@]}" -p "$RES?" | grep -q Yes >/dev/null || exit
 
 case "$RES" in
-  suspend)    systemctl suspend     ;;
-  reboot)     systemctl reboot      ;;
-  poweroff)   systemctl poweroff -i ;;
-  exit.i3)    i3-msg exit ;;
-  reload.i3)
-    i3-msg reload
-    trap - exit
-    $NOTIFY -t 2500 -u low "$RES [DONE]"
-  ;;
-  restart.i3)
-    i3-msg restart
-    trap - exit
-    $NOTIFY -t 2500 -u low "$RES [DONE]"
-  ;;
+  suspend)  systemctl suspend     ;;
+  reboot)   systemctl reboot      ;;
+  poweroff) systemctl poweroff -i ;;
+  i3-reload)  i3-msg reload;  notify-send -eu low "i3 configuration reloaded" ;;
+  i3-restart) i3-msg restart; notify-send -eu low "i3 restarted"              ;;
+  i3-exit)    i3-msg exit ;;
 esac
